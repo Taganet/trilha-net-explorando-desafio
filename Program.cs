033cf970751b0e0c0920fc8b806931db81baa569
Elielder Oliveira <taganet.info@gmail.com>
2023-01-01 20:53:01 -0300

    Desafio concluido com Loop para testes!

--- Program.cs
+++ Program.cs
@@ -6,20 +6,33 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+//Loop para testes
+int quantidadeParaHospedar = 4;
+for (int i = 1; i <= quantidadeParaHospedar; i++)
+{
+    Pessoa p = new Pessoa(nome: "Hospede " + i);
+    hospedes.Add(p);
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+    //Console.WriteLine(p.Nome);
+}
+
+// Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+// Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
+// hospedes.Add(p1);
+// hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+//Quebra de linha adicionada no final da execução
+Console.WriteLine("\n");
